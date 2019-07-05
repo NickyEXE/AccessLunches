d1737c3e6be4bd7d5941bcc2ef02a492f29ad694
NickyEXE <dovern42@gmail.com>
2019-07-05 14:48:57 -0400

    Restaurants are being pulled from Yelp on seed

---
+++ app/models/restaurant.rb
@@ -0,0 +1,30 @@
+class Restaurant < ApplicationRecord
+
+
+
+
+
+
+    # Called on seeding only
+
+    def self.get_restaurants_from_yelp
+        url = "https://api.yelp.com/v3/businesses/search"
+        params = {
+            latitude: 40.700866,
+            longitude: -73.989666,
+            limit: 50,
+            term: "lunch",
+            radius: 300
+        }
+        response = HTTP.auth("Bearer #{ENV['API_KEY']}").get(url, params: params)
+        businesses = response.parse["businesses"]
+        businesses.each do |business|
+            self.create_business_from_yelp_data(business)
+        end
+    end
+
+    def self.create_business_from_yelp_data(business)
+        Restaurant.create(name: business["name"], url: business["url"], image_url: business["image_url"], lat: business["coordinates"]["latitude"], long: business["coordinates"]["latitude"], address: business["location"]["address1"], kind_of_food: business["categories"][0]["title"])
+    end
+
+end
