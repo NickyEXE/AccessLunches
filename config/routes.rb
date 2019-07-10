1685b25209e6945e1d52cc55bcf9358dfb8a95ac
NickyEXE <dovern42@gmail.com>
2019-07-10 12:38:06 -0400

    continued to delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :restaurants, only: [:index, :show]
-  resources :users, only: [:new, :create]
-  resources :reviews, only: [:create, :destroy]
-
-  post '/restaurants', to: "restaurants#find", as: "find_restaurant"
   get '/login', to: "auth#new", as: "login"
   post '/login', to: "auth#create", as: "authorize"
   delete '/login', to: "auth#destroy", as: "logout"
