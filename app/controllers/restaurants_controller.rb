bdb5733018bda73a96a9e0bb8c373a8fe44f492c
NickyEXE <dovern42@gmail.com>
2019-07-10 12:34:21 -0400

    more stuff deleted

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,20 +1,20 @@
 class RestaurantsController < ApplicationController
     before_action :authorized
 
-    def index
-        @restaurants = Restaurant.all.sort_by{|restaurant| restaurant.name}
-    end
+    # def index
+    #     @restaurants = Restaurant.all.sort_by{|restaurant| restaurant.name}
+    # end
 
-    def show
-        @restaurant = Restaurant.find_by(id: params[:id])
-        @reviews = @restaurant.reviews
-        @review = Review.new()
-    end
+    # def show
+    #     @restaurant = Restaurant.find_by(id: params[:id])
+    #     @reviews = @restaurant.reviews
+    #     @review = Review.new()
+    # end
 
 
-    def find
-        redirect_to Restaurant.find_by(id: params[:restaurant][:restaurant_id])
-    end
+    # def find
+    #     redirect_to Restaurant.find_by(id: params[:restaurant][:restaurant_id])
+    # end
 
 
 
